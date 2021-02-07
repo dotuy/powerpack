f44a9ef65997bdceafa9d19d369ba984034ef621
Bart Calixto <bart@calixto.uy>
2021-02-07 08:07:46 -0300

    Add project files.

---
+++ BlazorState/AppState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorState
+{
+    public class AppState
+    {
+        readonly Dictionary<Type, object> States = new Dictionary<Type, object>();
+
+        internal T Get<T>() where T : new()
+        {
+            if (!States.ContainsKey(typeof(T)))
+            {
+                States.Add(typeof(T), new T());
+            }
+            var state = States[typeof(T)];
+            return (T)state;
+        }
+
+        internal void Set<T>(T state)
+        {
+            States[typeof(T)] = state;
+            OnStateUpdated(EventArgs.Empty);
+        }
+
+        protected virtual void OnStateUpdated(EventArgs e)
+        {
+            StateUpdated?.Invoke(this, e);
+        }
+
+        public event EventHandler StateUpdated;
+
+    }
+}
---
+++ BlazorState/StateComponent.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorState
+{
+    public class StateComponent : ComponentBase
+    {
+        [Inject] public AppState AppState { get; set; }
+    }
+
+    public class StateComponent<T> : StateComponent, IDisposable where T : new()
+    {
+        public T State { get { return AppState.Get<T>(); } }
+
+        public void SetState(T state)
+        {
+            AppState.Set(state);
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                AppState.StateUpdated += Refresh;
+            }
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            Console.WriteLine("state has changed"); StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            AppState.StateUpdated -= Refresh;
+
+        }
+    }
+}
